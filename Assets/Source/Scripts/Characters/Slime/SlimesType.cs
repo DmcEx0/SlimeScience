2c19e4d0017e567988fbfe5295560c0115184703
Maksim Smolko <smolko.m@amayasoft.uz>
2024-07-29 19:01:47 +0300

    Add slime type

---
+++ Assets/Source/Scripts/Characters/Slime/SlimesType.cs
@@ -0,0 +1,10 @@
+namespace SlimeScience
+{
+    public enum SlimesType
+    {
+        Default = 0,
+        Big = 1,
+        Teleported = 2,
+        Boss = 3
+    }
+}
--- Assets/Source/Scripts/Configs/BlockData.cs
+++ Assets/Source/Scripts/Configs/BlockData.cs
@@ -11,11 +11,13 @@ namespace SlimeScience.Configs
         [SerializeField] private int _neededAmountToOpen;
         [SerializeField] private float _maxRangePosX;
         [SerializeField] private float _maxRangePosZ;
+        [SerializeField] private SlimesType _slimesType;
 
         public int BombAmount => _bombAmount; 
         public int SlimeAmount => _slimeAmount;
         public int NeededAmountToOpen => _neededAmountToOpen;
         public float MaxRangePosX => _maxRangePosX;
         public float MaxRangePosZ => _maxRangePosZ;
+        public SlimesType SlimesType => _slimesType;
     }
 }
